889dd8916225333a3d03705643dab14ee0da2a90
Igor Batista <igorbatista.dev@gmail.com>
2025-12-01 19:15:08 -0300

    chore: Reduz recursos para rodar local

---
+++ src/main/java/com/confetaria/confetaria_backend/controller/DevOpsController.java
@@ -0,0 +1,25 @@
+package com.confetaria.confetaria_backend.controller;
+
+import com.confetaria.confetaria_backend.model.Cliente;
+import com.confetaria.confetaria_backend.model.Material;
+import com.confetaria.confetaria_backend.service.interfaces.ClienteService;
+import com.confetaria.confetaria_backend.service.interfaces.MaterialService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/devops")
+public class DevOpsController {
+
+    public DevOpsController() {
+    }
+
+    @GetMapping
+    public String listarClientes() {
+        return "Olá Ivo!";
+    }
+}
